a3849f1ef15b61023898c84b07d4d090cc41dc23
LuizNeto <luizneton13@gmail.com>
2024-12-10 12:21:31 -0300

    adicionando os testes para MainSection e RequestError

---
+++ src/components/main/__test__/MainSection.spec.tsx
@@ -0,0 +1,15 @@
+import { render, screen } from "@testing-library/react"
+import MainSection from "../MainSection"
+
+jest.mock("../MiddleSection", ()=> ()=> <div>middleSection</div>)
+jest.mock("../LeftSection", ()=> ()=> <div>leftSection</div>)
+jest.mock("../RightSection", ()=> ()=> <div>rightSection</div>)
+
+describe("MainSection", () => {
+    it("testing wether MainSection is being rendered", () => {
+        render(<MainSection />)
+        expect(screen.getByText("middleSection")).toBeInTheDocument()
+        expect(screen.getByText("leftSection")).toBeInTheDocument()
+        expect(screen.getByText("rightSection")).toBeInTheDocument()
+    })
+})
---
+++ src/components/main/__test__/RequestError.spec.tsx
@@ -0,0 +1,11 @@
+import { render, screen } from "@testing-library/react"
+import RequestError from "../RequestError"
+
+describe("RequestError", () => {
+    it("testing wether RequestError is being rendered", () => {
+        render(<RequestError />)
+        expect(screen.getByText("Ops...")).toBeInTheDocument()
+        expect(screen.getByText("não foi possível encontrar")).toBeInTheDocument()
+        expect(screen.getByText("tente novamente.")).toBeInTheDocument()
+    })
+})
